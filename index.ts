d5ff543c44cc30d5ebaec342519e1252775cbcdc
Carl Schroedl <carlschroedl@gmail.com>
2024-06-01 01:49:30 +0000

    Starting to add oktokit

---
+++ index.ts
@@ -0,0 +1,23 @@
+import { Octokit } from "@octokit/core";
+import { paginateGraphQL } from "@octokit/plugin-paginate-graphql";
+
+const MyOctokit = Octokit.plugin(paginateGraphQL);
+const octokit = new MyOctokit({ auth: "secret123" });
+
+const { repository } = await octokit.graphql.paginate(
+    `query paginate($cursor: String) {
+      repository(owner: "octokit", name: "rest.js") {
+        issues(first: 10, after: $cursor) {
+          nodes {
+            title
+          }
+          pageInfo {
+            hasNextPage
+            endCursor
+          }
+        }
+      }
+    }`,
+  );
+  
+  console.log(`Found ${repository.issues.nodes.length} issues!`);
