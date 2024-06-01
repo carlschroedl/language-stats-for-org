2ea156d01a0349da985dc2ed39d6e3cde3532e81
Carl Schroedl <carlschroedl@gmail.com>
2024-06-01 01:58:24 +0000

    borked

--- index.ts
+++ index.ts
@@ -1,8 +1,39 @@
+// https://github.com/octokit/plugin-paginate-graphql.js
+
 import { Octokit } from "@octokit/core";
 import { paginateGraphQL } from "@octokit/plugin-paginate-graphql";
 
 const MyOctokit = Octokit.plugin(paginateGraphQL);
 const octokit = new MyOctokit({ auth: "secret123" });
+async function main() {
+const query = `
+{
+  repositoryOwner(login: "my_org_name") {
+    ... on Organization {
+      repositories(first: 100) {
+        totalCount
+        pageInfo {
+          endCursor
+          hasNextPage
+        }
+        nodes {
+          repo_url: url
+          languages(first: 100) {
+            totalSize
+            edges {
+              size
+              node {
+                name
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+`
 
 const { repository } = await octokit.graphql.paginate(
     `query paginate($cursor: String) {
@@ -21,3 +52,5 @@ const { repository } = await octokit.graphql.paginate(
   );
   
   console.log(`Found ${repository.issues.nodes.length} issues!`);
+}
+main();
